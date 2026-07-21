4e70f647c11b235142b1d123f93b72a17c7ee0e3
Dage10 <davidgelmacorral@gmail.com>
2026-07-21 20:40:57 +0200

    Initial commit

---
+++ src/test/java/com/david/monitoring/MonitoringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
